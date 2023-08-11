ec5613f105e6804659fbcef746091c5a3cf9c287
jlarata <ariel@cfee.gob.ar>
2023-08-10 21:56:42 -0300

    5

--- Assets/Scripts/MaloController.cs
+++ Assets/Scripts/MaloController.cs
@@ -18,18 +18,8 @@ public class MaloController : MonoBehaviour
     public GameObject posta11;
     public GameObject posta12;
 
-    public bool istramo1;
-    public bool istramo2;
-    public bool istramo3;
-    public bool istramo4;
-    public bool istramo5;
-    public bool istramo6;
-    public bool istramo7;
-    public bool istramo8;
-    public bool istramo9;
-    public bool istramo10;
-    public bool istramo11;
-    public bool istramo12;
+    public int currentTramo;
+
     
     void Start()
     {
@@ -47,7 +37,7 @@ public class MaloController : MonoBehaviour
         posta11 = GameObject.Find("Posta11");
         posta12 = GameObject.Find("Posta12");
 
-        istramo1 = true;
+        currentTramo = 1;
     }
     void Update()
     {
@@ -62,116 +52,78 @@ public class MaloController : MonoBehaviour
 
     public void CheckTramo()
     {
-        if (transform.position == posta01.transform.position)
+        if (transform.position == posta01.transform.position || 
+        transform.position == posta02.transform.position || 
+        transform.position == posta03.transform.position || 
+        transform.position == posta04.transform.position || 
+        transform.position == posta05.transform.position || 
+        transform.position == posta06.transform.position || 
+        transform.position == posta07.transform.position || 
+        transform.position == posta08.transform.position || 
+        transform.position == posta09.transform.position || 
+        transform.position == posta10.transform.position || 
+        transform.position == posta11.transform.position)
         {
-            istramo1 = false;
-            istramo2 = true;
-        }
-        if (transform.position == posta02.transform.position)
-        {
-            istramo2 = false;
-            istramo3 = true;
-        }
-        if (transform.position == posta03.transform.position)
-        {
-            istramo3 = false;
-            istramo4 = true;
-        }
-        if (transform.position == posta04.transform.position)
-        {
-            istramo4 = false;
-            istramo5 = true;
-        }
-        if (transform.position == posta05.transform.position)
-        {
-            istramo5 = false;
-            istramo6 = true;
-        }
-        if (transform.position == posta06.transform.position)
-        {
-            istramo6 = false;
-            istramo7 = true;
-        }
-        if (transform.position == posta07.transform.position)
-        {
-            istramo7 = false;
-            istramo8 = true;
-        }
-        if (transform.position == posta08.transform.position)
-        {
-            istramo8 = false;
-            istramo9 = true;
-        }
-        if (transform.position == posta09.transform.position)
-        {
-            istramo9 = false;
-            istramo10 = true;
-        }
-        if (transform.position == posta10.transform.position)
-        {
-            istramo10 = false;
-            istramo11 = true;
-        }
-        if (transform.position == posta11.transform.position)
-        {
-            istramo11 = false;
-            istramo12 = true;
-        }
-        if (transform.position == posta12.transform.position)
-        {
-            istramo12 = false;
-            Impact();
+            currentTramo += 1;
         }
         
+        
+        if (transform.position == posta12.transform.position)
+        {
+            currentTramo += 1;
+            Impact();
+        }
     }
+
+    
     
     public void MoveToPosta()
     {
-        if (istramo1)
+        if (currentTramo == 1)
         {
             transform.position = Vector3.MoveTowards(transform.position, posta01.transform.position, Time.deltaTime*4);
         }
-        if (istramo2)
+        if (currentTramo == 2)
         {
             transform.position = Vector3.MoveTowards(transform.position, posta02.transform.position, Time.deltaTime*4);
         }
-        if (istramo3)
+        if (currentTramo == 3)
         {
             transform.position = Vector3.MoveTowards(transform.position, posta03.transform.position, Time.deltaTime*4);
         }
-        if (istramo4)
+        if (currentTramo == 4)
         {
             transform.position = Vector3.MoveTowards(transform.position, posta04.transform.position, Time.deltaTime*4);
         }
-        if (istramo5)
+        if (currentTramo == 5)
         {
             transform.position = Vector3.MoveTowards(transform.position, posta05.transform.position, Time.deltaTime*4);
         }
-        if (istramo6)
+        if (currentTramo == 6)
         {
             transform.position = Vector3.MoveTowards(transform.position, posta06.transform.position, Time.deltaTime*4);
         }
-        if (istramo7)
+        if (currentTramo == 7)
         {
             transform.position = Vector3.MoveTowards(transform.position, posta07.transform.position, Time.deltaTime*4);
         }
-        if (istramo8)
+        if (currentTramo == 8)
         {
             transform.position = Vector3.MoveTowards(transform.position, posta08.transform.position, Time.deltaTime*4);
         }
-        if (istramo9)
+        if (currentTramo == 9)
         {
             transform.position = Vector3.MoveTowards(transform.position, posta09.transform.position, Time.deltaTime*4);
         }
-        if (istramo10)
+        if (currentTramo == 10)
         {
             transform.position = Vector3.MoveTowards(transform.position, posta10.transform.position, Time.deltaTime*4);
         }
-        if (istramo11)
+        if (currentTramo == 11)
         {
             transform.position = Vector3.MoveTowards(transform.position, posta11.transform.position, Time.deltaTime*4);
         }
-        if (istramo12)
+        if (currentTramo == 12)
         {
             transform.position = Vector3.MoveTowards(transform.position, posta12.transform.position, Time.deltaTime*4);
         }
